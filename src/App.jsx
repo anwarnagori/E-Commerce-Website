a71e8c6202d32d981518e4fdce8b2a21775b9126
anwarnagori <ar1498137@gmail.com>
2024-12-10 07:40:29 +0500

    started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+    </div>
+  );
+};
+
+export default App;
