563fe0611ef8c9c3ad8d6d4c6f2f3f7995c89733
anwarnagori <ar1498137@gmail.com>
2024-12-12 08:08:43 +0500

    update-code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world.</h1>
-    </div>
+    <>
+     <div className="header">
+        <nav></nav>
+     </div>
+    </>
   );
 };
 
