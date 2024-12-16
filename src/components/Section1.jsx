9721d188870175eb8a2b3c703b87ac089711770f
anwarnagori <ar1498137@gmail.com>
2024-12-16 23:08:44 +0500

    update-code

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const Section1 = () => {
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>MY FIRST E-COMMERCE PROJECT</h1>
-      </div>
-    </div>
-  );
+  <div>
+    
+  </div>
+  )
 };
 
 export default Section1;
