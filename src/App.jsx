e6026eaebd9a076318499eb01ab0876d163c4c05
anwarnagori <ar1498137@gmail.com>
2024-12-19 23:11:36 +0500

    update-code

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "../assets/css/style.css";
 import "../assets/css/responsive.css";
 import Header from "./components/Header";
 import First from "./components/First";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <First />
+      <Footer />
     </>
   );
 };
