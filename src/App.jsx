7ef11d6626f5c72a3c8e6d50bbb9a4e3498c9753
anwarnagori <ar1498137@gmail.com>
2024-12-22 02:49:34 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../assets/css/style.css";
-import "../assets/css/responsive.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
